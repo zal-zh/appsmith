cbbdbba237ce339556548ffdfcfe43e99f4c91ba
zhang al0208 <zhangal0208@digiwin.com>
2025-03-31 08:42:04 +0000

    增加表单

--- pages/财务充电站/jsobjects/JSObject1/JSObject1.js
+++ pages/财务充电站/jsobjects/JSObject1/JSObject1.js
@@ -1,20 +1,7 @@
 export default {
-	myVar1: [],
-	myVar2: {},
-	myFun1: async () => {
-		const data = await basicJSON.run();
-		const list = [];
-		for(const iterator in data){
-			if (typeof data[iterator] === 'string') {
-				list.push({ label: data[iterator], value: iterator });
-			}
-
-		}
-		console.log('list', list)
-		return list;
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
+	saveFormData: async () => {
+		const data = JSONForm1.formData;
+		console.log('data', data);
+		console.log(appsmith.URL)
 	}
 }
