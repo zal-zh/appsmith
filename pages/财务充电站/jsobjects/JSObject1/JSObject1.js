936ea9af7a4323a38621001c3dce0bed490bdd69
360372331 <360372331@qq.com>
2025-04-02 03:12:49 +0000

    q

--- pages/财务充电站/jsobjects/JSObject1/JSObject1.js
+++ pages/财务充电站/jsobjects/JSObject1/JSObject1.js
@@ -1,6 +1,11 @@
 export default {
 	initData: () => {
 		postWindowMessage({ type: 'FIXED_TOP_FULL_WIDTH', class: 't--widget-header' }, 'window', "*");
+		MyCustomEvent.emit('MY_FIXED_TOP_FULL_WIDTH', {
+			detail: { type: 'FIXED_TOP_FULL_WIDTH', class: 't--widget-header' },
+			bubbles: true,
+			cancelable: true
+		});
 	},
 	saveFormData: async () => {
 		formJson.setText(appsmith.URL.fullPath + '\n' + JSON.stringify(JSONForm1.formData));
@@ -9,9 +14,19 @@ export default {
 		if (Tabs1.selectedTab === '领取资料') {
 			navigateTo('https://work.weixin.qq.com/ca/cawcde4d453a8ae728?customer_channel=link-3743-#%E8%8E%B7%E5%AE%A2%E6%89%8B%E6%9C%BA%E5%8F%B7#', {}, 'NEW_WINDOW');
 		} else if (Tabs1.selectedTab === '本月直播') {
-			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-imagecarousel' }, '*', "*");
+			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-imagecarousel' }, 'window', "*");
+			MyCustomEvent.emit('MY_SCROLL_DOM_BY_CLASS', {
+				detail: { type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-imagecarousel' },
+				bubbles: true,
+				cancelable: true
+			});
 		} else if (Tabs1.selectedTab === '精选课程') {
-			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-title2' }, '*', "*");
+			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-title2' }, 'window', "*");
+			MyCustomEvent.emit('MY_SCROLL_DOM_BY_CLASS', {
+				detail: { type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-title2' },
+				bubbles: true,
+				cancelable: true
+			});
 		}
 
 	}
