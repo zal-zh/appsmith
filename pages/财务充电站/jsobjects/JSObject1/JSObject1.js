e8424a5ba16c77598a3d5a1d6af9ebefeef04b53
zhang al0208 <zhangal0208@digiwin.com>
2025-04-02 02:39:03 +0000

    11

--- pages/财务充电站/jsobjects/JSObject1/JSObject1.js
+++ pages/财务充电站/jsobjects/JSObject1/JSObject1.js
@@ -9,9 +9,9 @@ export default {
 		if (Tabs1.selectedTab === '领取资料') {
 			navigateTo('https://work.weixin.qq.com/ca/cawcde4d453a8ae728?customer_channel=link-3743-#%E8%8E%B7%E5%AE%A2%E6%89%8B%E6%9C%BA%E5%8F%B7#', {}, 'NEW_WINDOW');
 		} else if (Tabs1.selectedTab === '本月直播') {
-			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-imagecarousel' }, 'window', "*");
+			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-imagecarousel' }, '*', "*");
 		} else if (Tabs1.selectedTab === '精选课程') {
-			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-title2' }, 'window', "*");
+			postWindowMessage({ type: 'SCROLL_DOM_BY_CLASS', class: 't--widget-title2' }, '*', "*");
 		}
 
 	}
